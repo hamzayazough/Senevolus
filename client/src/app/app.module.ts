15dc74b9c8d937ead2253a327db8bc0c6759a5f8
Samyar Janatian <samyar.mahmoudi@polymtl.ca>
2025-01-25 23:27:58 -0500

    feat: implement addr related classes

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -15,16 +15,19 @@ import { ListTaskComponent } from './common/list-task/list-task.component';
 import { AuthService } from '@auth0/auth0-angular';
 import { ElderTaskPageComponent } from './pages/elder-task-page/elder-task-page.component';
 import { ChatComponent } from './common/chat/chat.component';
-import {MatTabsModule} from '@angular/material/tabs';
+import { MatTabsModule } from '@angular/material/tabs';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { ChatService } from './services/chat-service/chat-service.service';
 import { VolunteerTaskPageComponent } from './pages/volunteer-task-page/volunteer-task-page.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { ShopComponent } from './pages/shop/shop.component';
 import { ShopProductComponent } from './pages/shop/shop-product/shop-product.component';
 import { CreateTaskComponent } from './pages/create-task/create-task.component';
+import { AddressAutocompleteComponent } from './common/address-autocomplete/address-autocomplete.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -41,7 +44,8 @@ import { CreateTaskComponent } from './pages/create-task/create-task.component';
     ProfileComponent,
     ShopComponent,
     ShopProductComponent,
-    CreateTaskComponent
+    CreateTaskComponent,
+    AddressAutocompleteComponent,
   ],
   imports: [
     BrowserModule,
@@ -49,13 +53,15 @@ import { CreateTaskComponent } from './pages/create-task/create-task.component';
     AppRoutingModule,
     FormsModule,
     MatTabsModule,
-    MatSidenavModule
+    MatSidenavModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
   ],
   providers: [
     provideClientHydration(),
     AuthService,
     ChatService,
-    provideAnimationsAsync()
+    provideAnimationsAsync(),
   ],
   bootstrap: [AppComponent],
 })
---
+++ client/src/app/interfaces/address.ts
@@ -0,0 +1,6 @@
+export interface Address {
+  addr_id: number;
+  name: string;
+  latitude: number;
+  longitude: number;
+}
