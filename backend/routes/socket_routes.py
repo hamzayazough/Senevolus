83713452bdc8c6489eb7ccc2ca663f5755d3bd82
hamza <hamza.yazough1992@gmail.com>
2025-01-26 06:58:47 -0500

    merge

--- backend/routes/socket_routes.py
+++ backend/routes/socket_routes.py
@@ -1,6 +1,70 @@
 from flask_socketio import emit, join_room
 from flask import Flask, request
 from models.user_model import get_user_by_id, add_user
+from models.task_model import get_tasks_by_elder, get_tasks_by_volunteer
+messages = [
+    {
+        'name' : 'Mina',
+        'message':'Hey',
+        'type' : 'received'
+    }, 
+    {
+        'name' : 'Samuel',
+        'message':'Hey1',
+        'type' : 'received'
+    }, 
+    {
+        'name' : 'Mina',
+        'message':'Hey2',
+        'type' : 'received'
+    }, 
+    {
+        'name' : 'Mina',
+        'message':'Hey3',
+        'type' : 'sent'
+    }, 
+    {
+        'name' : 'Samuel',
+        'message':'Hey4',
+        'type' : 'received'
+    }, 
+    {
+        'name' : 'Mina',
+        'message':'Hey',
+        'type' : 'received'
+    }, 
+    {
+        'name' : 'Mina',
+        'message':'Hey',
+        'type' : 'received'
+    }, 
+    {
+        'name' : 'Mina',
+        'message':'Hey',
+        'type' : 'received'
+    }, 
+    {
+        'name' : 'Mina',
+        'message':'Hey',
+        'type' : 'received'
+    }, 
+    {
+        'name' : 'Mina',
+        'message':'Hey',
+        'type' : 'received'
+    }, 
+    {
+        'name' : 'Samuel',
+        'message':'Hey',
+        'type' : 'received'
+    }, 
+    {
+        'name' : 'Mina',
+        'message':'Hey',
+        'type' : 'received'
+    }, 
+]
+
 
 def socketio_handlers(socketio):
     @socketio.on('send_message')
@@ -11,8 +75,16 @@ def socketio_handlers(socketio):
 
     @socketio.on('connect_getuser')
     def connect_getuser(data):
+        print("getting connect get user")
         userdata = get_user_by_id(data)
+        print(userdata)
         emit('connect_getuser', userdata, to=request.sid)
+
+    @socketio.on('getuser')
+    def connect_getuser(data):
+        print("getting get user")
+        userdata = get_user_by_id(data)
+        emit('gotuser', userdata, to=request.sid)
 
     @socketio.on('createUser')
     def userCreated(data):
@@ -48,6 +120,25 @@ def socketio_handlers(socketio):
     @socketio.on('connect')
     def handle_connect():
         print("Client connected")
+
+    @socketio.on('getListElder')
+    def getListElder(data):
+        taskList = get_tasks_by_elder(data)
+        emit('gotListElder', {'task': taskList})
+    
+    @socketio.on('getListVolunteer')
+    def getListVolunteer(data):
+        print(data)
+        taskList = get_tasks_by_volunteer(data)
+        emit('gotListVolunteer', {'task': taskList}, to=request.sid)
+    
+    @socketio.on('getMessages')
+    def getMessages():
+        emit('gotMessages', messages, to=request.sid)
+
+    @socketio.on('sendMessage')
+    def sendMessage(data):
+        emit('receivedMessage', data, include_self=False)
 
     @socketio.on('disconnect')
     def handle_disconnect():
--- client/src/app/services/chat-service/chat-service.service.ts
+++ client/src/app/services/chat-service/chat-service.service.ts
@@ -6,20 +6,38 @@ import { SocketService } from '../socket.service';
 })
 export class ChatService {
   opened : boolean = false;
-  chatMessages : [{name : string, message : string}] = [{
+  chatMessages : any = [{
     name : 'mina',
-    message : 'LOLOLOL'
-  }]
+    message : 'LOLOLOL',
+    type : 'received'
+  },
+  {
+    name : 'alfredo',
+    message : 'LOLOLOL',
+    type : 'sent'
+  }];
 
 
   constructor(
     private socketService: SocketService
   ) { 
-    
+    this.initializeEvents();
+    this.socketService.send('getMessages');
+  }
+
+  sendMessage(name : string, message : string) {
+    this.chatMessages.push({name: name, message : message, type : 'sent'});
+    this.socketService.send('sendMessage', {name: name, message: message, type : 'received'})
   }
 
   initializeEvents() {
+    this.socketService.on('receiveMessage', (message:any) => {
+      this.chatMessages.push({name:message.name, message:message.message, type:'received'});
+    });
 
+    this.socketService.on('gotMessages', (messages:[{name : string, message : string}]) => {
+      this.chatMessages = messages;
+    });
   }
 
   toggleChat() {
