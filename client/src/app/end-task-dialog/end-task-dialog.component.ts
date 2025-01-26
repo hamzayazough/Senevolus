9f7d34eec1ba086418fa923ae6f7e37e64c683c9
Anis <anisjjo@gmail.com>
2025-01-26 11:33:24 -0500

    final push camera

--- client/src/app/end-task-dialog/end-task-dialog.component.ts
+++ client/src/app/end-task-dialog/end-task-dialog.component.ts
@@ -68,7 +68,7 @@ export class EndTaskDialogComponent {
       const selfiePhotoFile = this.dataURLtoFile(this.capturedImage, 'idPhoto.png');
   
       // Upload the files using the FileUploadService
-      this.fileUploadService.uploadSelfiePhoto(selfiePhotoFile).subscribe(
+      this.fileUploadService.uploadSelfiePhoto(selfiePhotoFile, this.capturedImage).subscribe(
         (response) => {
           console.log('Upload successful:', response);
           // Proceed with form submission
